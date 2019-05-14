845ee8b6d05c25ba573a5cee073af2ac6bb84f47
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-14 11:33:22 +0530

    p6.py

---
+++ p6.py
@@ -0,0 +1,18 @@
+MAX_chars=256
+def isographi(string1,string2):
+a=len(string1)
+b=len(string2)
+if a in b:
+return false
+marked=[false]=>MAX_chars
+map=[-1]=MAX_chars
+for i in range(a):
+if map(ord(string([i])]==-1:
+if marked[ord(string2[i])==true:
+return false
+marked[ord(string2[i])]=true
+map{ord(string[i]}=string2[i]
+elif map[ord(string[(i])]=string2[i]:
+return false
+return true
+print(isomorphic(Input('enter the str1'),input('enter the str1')))
