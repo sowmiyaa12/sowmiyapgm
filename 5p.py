5667b2c48c6d83e759904cf1471ec4e68afd7264
sowmiyaa12 <49434427+sowmiyaa12@users.noreply.github.com>
2019-05-14 11:25:06 +0530

    5p.py

---
+++ 5p.py
@@ -0,0 +1,11 @@
+class integer:
+def roman(self,s):
+n=('1','s's'x''10''c'100)
+int=0
+for i in range(len(S)):
+if i>0 and n[s[i]]>nn[s(i-1]]:
+int==n[s[i]]-2+n[s[i]]
+else:
+int +=n[s[i]]
+return int
+print(integer().roman(input('enter the roman')))
